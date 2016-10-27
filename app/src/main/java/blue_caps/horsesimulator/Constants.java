27eb5f3f38563f6c351669dc2b17484fadcc1429
Alexander Udalov <alexudalovvvvv@outlook.com>
2016-10-27 22:57:13 +0300

    Написал предварительный интерфейс

---
+++ app/src/main/java/blue_caps/horsesimulator/Constants.java
@@ -0,0 +1,111 @@
+package blue_caps.horsesimulator;
+
+
+/**
+ * Created by alex on 27.10.16.
+ */
+
+public interface Constants {
+    int wasStepDownHappiness = 2,
+            wasStepDownSatiety = 3,
+            wasStepDownStamina = 3,
+
+    eatGrassUpSatiety = 4,
+            eatGrassDownStamina = 6,
+
+    stealingFoodUpSatiety = 8,
+            stealingFoodDownStamina = 8,
+            stealingFoodDownHappiness = 3,
+            stealingFoodUpRespectHorses = 4,
+            stealingFoodDownRespectPeoples = 4,
+
+    beggingSugarUpSatiety = 2,
+            beggingSugarDownStamina = 6,
+            beggingSugarUpHappiness = 4,
+            beggingSugarDownRespectHorses = 2,
+            beggingSugarUpRespectPeoples = 2,
+
+    askForFoodUpSatiety = 8,
+            askForFoodUpHappiness = 4,
+            askForFoodDownStamina = 6,
+
+    eatAppleUpHappiness = 20,
+            eatAppleUpSatiety = 50,
+
+    haveSleepUpStaminaTABOR = 6,
+            haveSleepDownSatietyTABOR = 3,
+            haveSleepUpStaminaWASTELAND = 8,
+            haveSleepDownSatietyWASTELAND = 4,
+            haveSleepUpStaminaCLEAR_FIELD = 10,
+            haveSleepDownSatietyCLEAR_FIELD = 5,
+            haveSleepUpStaminaMEADOWS = 15,
+            haveSleepDownSatietyMEADOWS = 7,
+            haveSleepUpStaminaPRAIRIE = 20,
+            haveSleepDownSatietyPRAIRIE = 10,
+            haveSleepUpHappinessPRAIRIE = 4,
+            haveSleepUpStaminaKAZAKHSTAN = 25,
+            haveSleepDownSatietyKAZAKHSTAN = 12,
+            haveSleepUpHappinessKAZAKHSTAN = 10,
+            haveSleepUpStaminaPADDOCK = 8,
+            haveSleepDownSatietyPADDOCK = 4,
+            haveSleepUpStaminaSTABLE = 10,
+            haveSleepDownSatietySTABLE = 5,
+            haveSleepUpStaminaRANCH = 15,
+            haveSleepDownSatietyRANCH = 7,
+            haveSleepUpStaminaHORSE_CLUB = 20,
+            haveSleepDownSatietyHORSE_CLUB = 10,
+            haveSleepUpHappinessHORSE_CLUB = 4,
+            haveSleepUpStaminaPRICATE_FARM = 20,
+            haveSleepDownSatietyPRICATE_FARM = 10,
+            haveSleepUpHappinessPRICATE_FARM = 4,
+
+    goToWateringUpStamina = 10,
+            goToWateringDownSatiety = 4,
+            goToWateringUpHappiness = 4,
+
+    goToDrinkersUpStamina = 10,
+            goToDrinkersDownSatiety = 4,
+            goToDrinkersUpHappiness = 4,
+
+    getMassageUpStamina = 14,
+            getMassageUpHappiness = 5,
+
+    swimInLakeUpStamina = 14,
+            swimInLakeUpHappiness = 5,
+
+    findAppleDownStamina = 15,
+            findAppleDownSatiety = 15,
+
+    plowedFieldDownStamina = 0,
+            plowedFieldDownSatiety = 0,
+            plowedFieldDownRespectHorses = 0,
+            plowedFieldUpRespectPeoples = 0,
+
+    drugLoadsDownStamina = 0,
+            drugLoadsDownSatiety = 0,
+            drugLoadsDownRespectHorses = 0,
+            drugLoadsUpRespectPeoples = 0,
+
+    knockCorralGateDownSatiety = 0,
+            knockCorralGateDownStamina = 0,
+            knockCorralGateDownRespectPeoples = 0,
+            knockCorralGateUpRespectHorses = 0,
+
+
+    participateHorseRaceDownSatiety = 0,
+            participateHorseRaceDownStamina = 0,
+            participateHorseRaceUpRespectHorses = 0,
+
+    bobMusclesDownSatiety = 0,
+            bobMusclesDownStamina = 0,
+
+    participateChampionshipDownStamina = 0,
+            participateChampionshipDownSatiety = 0,
+            participateChampionshipUpRespectHorses = 0,
+            participateChampionshipUpRespectPeoples = 0,
+            participateChampionshipUpHappiness = 0,
+
+    participateChampionshipDownRespectHorses = 0,
+            participateChampionshipDownRespectPeoples = 0,
+            participateChampionshipDownHappiness = 0;
+}
--- app/src/main/java/blue_caps/horsesimulator/MainFragment.java
+++ app/src/main/java/blue_caps/horsesimulator/MainFragment.java
@@ -1,10 +1,30 @@
 package blue_caps.horsesimulator;
 
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by alexu on 27.10.2016.
  */
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        return view;
+    }
 }
