fb4da9c196f736706173ef5912513a073f0c7f02
Alexandr Arhipov <arhip.sasha.26@yandex.ru>
2016-10-31 15:57:13 +0300

    Добавил страницу №2(2)

--- app/src/main/java/blue_caps/horsesimulator/Page_2.java
+++ app/src/main/java/blue_caps/horsesimulator/Page_2.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -14,23 +15,69 @@ import java.util.Random;
  * Created by alexu on 30.10.2016.
  */
 
-public class Page_2 extends Fragment {
-    public final static String TITLE = "Pisos_2";
-    int backColor;
+public class Page_2 extends StepFragment {
+    private Button
+        buttonEatGrass,
+        buttonStealingFood,
+        buttonBeggingSugar,
+        buttonAskForFood,
+        buttonEatApple;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Random rnd = new Random();
-        backColor = Color.argb(40, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.page_2, null);
-        TextView tvPage = (TextView) view.findViewById(R.id.tvPage);
-        tvPage.setText(TITLE);
-        tvPage.setBackgroundColor(backColor);
+
+        buttonEatGrass     = (Button) view.findViewById(R.id.button_eat_grass);
+        buttonStealingFood = (Button) view.findViewById(R.id.button_stealing_food);
+        buttonBeggingSugar = (Button) view.findViewById(R.id.button_begging_sugar);
+        buttonAskForFood   = (Button) view.findViewById(R.id.button_ask_for_food);
+        buttonEatApple     = (Button) view.findViewById(R.id.button_eat_apple);
+
+        buttonEatGrass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                step();
+                MainActivity.controller.eatGrass();
+            }
+        });
+
+        buttonStealingFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                step();
+                MainActivity.controller.stealingFood();
+            }
+        });
+
+        buttonBeggingSugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                step();
+                MainActivity.controller.beggingSugar();
+            }
+        });
+
+        buttonAskForFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                step();
+                MainActivity.controller.askForFood();
+            }
+        });
+
+        buttonEatApple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                step();
+                MainActivity.controller.eatApple();
+            }
+        });
 
         return view;
     }
